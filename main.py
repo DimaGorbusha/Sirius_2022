d2933c356893766ffa7fbf59be41bb9877fcdeee
DmitriyGorbunov1 <dmitrgor1@yandex.ru>
2022-01-14 11:46:36 +0500

    Second commit

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 #----------------------main----------------------
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
-
-if __name__ == "__main__":
-	app.run(debug = True)
 
 
 @app.route("/")
 def index():
+	print(url_for('index'))
 	return "index"
 
 
-@app.route("/")
-def penis():
+@app.route("/admin")
+def admin():
+	print(url_for('admin'))
 	return "bebras"
+
+
+@app.route("/test/<int:test_number>")
+def test_info(test_number):
+	return f"Номер теста: {test_number}"
+
+
+@app.route("/create_test")
+def create_test():
+	return "abeba"
+
+
+if __name__ == "__main__":
+	app.run(debug = True)
