351a4b076616667145a74e25f3779186a5578616
DmitriyGorbunov1 <dmitrgor1@yandex.ru>
2022-01-21 10:00:08 +0500

    DataBase upgrade

--- main_site.py
+++ main_site.py
@@ -10,42 +10,44 @@ password = "17091857"
 
 @app.route("/")
 def index():
-	return render_template("frontend\public\index.html")
+	
+	# return render_template("frontend\public\index.html")
 
 
 @app.route("/list-tests")
 def list_test():
-	return render_template("frontend\public\index.html")
+
+	# return render_template("frontend\public\index.html")
 
 
 @app.route("/test-detail/<int:test_number>")
 def show_test_detail(index):
 	
-	return render_template("frontend\public\index.html", json_data[index])
+	# return render_template("frontend\public\index.html", json_data[index])
 
 
 
 @app.route("/create-test", methods=["POST", "GET"])
 def create_test():
-	return render_template("frontend\public\index.html")
+	# return render_template("frontend\public\index.html")
 
 
 @app.route("/sign-up", methods=["POST", "GET"])
 def sign_up():
 	if 'userLogged' in session:
-		json_data = {
-			"is_logged": True
-		}
-		return redirect(url_for("/list-tests"))
+		# json_data = {
+		# 	"is_logged": True
+		# }
+		# # return redirect(url_for("/list-tests"))
 
 	elif request.method == 'POST' and request.form['password'] == password:
 		system_logger_write
-		json_data = {
-			"is_logged": True
-		}
-		return redirect(url_for("/list-tests"))
+		# json_data = {
+		# 	"is_logged": True
+		# }
+		# # return redirect(url_for("/list-tests"))
 
-	return render_template('index.html')
+	# return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
